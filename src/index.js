cd384e308b6c1f0c596386328d08f03742376edf
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-12-17 01:06:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const myInput = document.querySelector('#search-box');
+
+myInput.addEventListener('input', search);
+
+function search() {
+    console.log(myInput.nodeValue);
+}
