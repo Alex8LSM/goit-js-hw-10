7e33cb34de0cb0a152930e188abfa4d7de68798d
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-12-28 01:47:26 +0200

    Finish

---
+++ src/fetchCountries.js
@@ -0,0 +1,47 @@
+import countriesList from './templates/countries-list.hbs';
+import countryInfo from './templates/country-info.hbs';
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  timeout: 5000,
+//   useIcon: false,
+  fontSize: '20px',
+  width: '350px',
+});
+export default function fetchCountries(Name) {
+  const countriesListHtml = document.querySelector('.country-list');
+  const countryInfoHtml = document.querySelector('.country-info');
+  if (Name == '') {
+    countryInfoHtml.innerHTML = '';
+    countriesListHtml.innerHTML = '';
+  } else {
+    fetch(
+      `https://restcountries.com/v3.1/name/${Name}?fields=name,capital,population,flags,languages`,
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(countries => {
+        if (countries.length > 10) {
+          countryInfoHtml.innerHTML = '';
+          countriesListHtml.innerHTML = '';
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (countries.length > 1) {
+          const markup = countriesList(countries);
+          countryInfoHtml.innerHTML = '';
+          countriesListHtml.innerHTML = markup;
+        } else {
+          const markup = countryInfo(countries[0]);
+          countriesListHtml.innerHTML = '';
+          countryInfoHtml.innerHTML = markup;
+        }
+      })
+      .catch(error => {
+        countryInfoHtml.innerHTML = '';
+        countriesListHtml.innerHTML = '';
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const myInput = document.querySelector('#search-box');
-
-myInput.addEventListener('input', search);
-
+myInput.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
 function search() {
-    console.log(myInput.nodeValue);
+  fetchCountries(myInput.value.trim());
 }
